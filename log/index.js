e90c7f034406b9fe334885e67f8bf813251c9efd
KagariSoft-Dev <dev@kagarisoft.com>
2023-04-30 20:19:16 -0300

    update 24 files

---
+++ log/index.js
@@ -0,0 +1,25 @@
+import winston from 'winston';
+
+const customLevelsOptions = {
+      levels: {
+            error: 1,
+            warning: 2,
+            http: 3,
+            debug: 4,
+      }
+}
+
+const logger = winston.createLogger({
+      levels: customLevelsOptions.levels,
+      transports: [
+            new winston.transports.Console({
+                  level: "debug"
+            })
+      ]
+})
+
+export const addLogger = (req, res, next) => {
+      req.logger = logger;
+      req.logger.papa(`${req.method} en ${req.url} - ${new Date().toLocaleTimeString()}`)
+      next();
+}
